ab68291e7fcc332afa4c5ffbf11760c8746cd639
nidhi <nidhi7485@gmail.com>
2022-10-12 14:04:27 +0530

    logging to an external file

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
+const logger = require('morgan')
+const fs = require('fs')
 const rateLimitMiddleware = require('./middleware/rateLimit')
 const collectionController = require('./controller/collectionController')
+
+app.use(
+  logger('combined', {
+    stream: fs.createWriteStream('./access.log', { flags: 'a' }),
+  })
+)
+app.use(logger('dev'))
 
 app.use(rateLimitMiddleware)
 app.get('/', (req, res) => {
